337b5824585f9bcf3e1cd2757d4c51e8203a5295
Paul deGrandis <paul.degrandis@gmail.com>
2013-01-08 18:59:52 -0800

    Worker uses the new blob interface; Also, don't ever use this

--- src/shoreleave/worker.cljs
+++ src/shoreleave/worker.cljs
@@ -3,9 +3,6 @@
             [shoreleave.browser.blob :as blob]
             [cljs.reader :as reader]))
 
-;; Turn blobbing on
-(blob/enable-blobbuilder!)
-
 (deftype WorkerFn [F eworker res-atom-vector]
   
   IWithMeta
@@ -33,8 +30,7 @@
   ;  (-notify-watches (.-res-atom-vector WFn) oldval newval))
   
   IHash
-  (-hash [WFn] (goog.getUid (.-f (.-F WFn))))
-  )
+  (-hash [WFn] (goog.getUid (.-f (.-F WFn)))))
 
 ;; DO NO USE THIS!!!!
 ;; I still need to find a way to inject/import in the JS behind namespaces
@@ -43,9 +39,7 @@
   (let [f-str (str f)
         fn-str (str "var swfn = " f-str ";"
                     "self.onmessage = function(c){self.postMessage(swfn(c.data));return c};")
-        bb (blob/blob-builder)
-        *b (conj! bb fn-str)
-        blobb (blob/blob bb)
+        blobb (blob/blob fn-str)
         agg (atom [])
         w (js/Worker. (blob/object-url! blobb))
         *w (set! (.-onmessage w) #(swap! agg conj (.-data %)))
